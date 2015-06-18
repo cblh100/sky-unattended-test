873699a663162148c86ff614309845e6bcd38c42
Chris Leeds-Harrison <chris@megadome.co.uk>
2015-06-18 21:23:36 +0100

    BillService to call through to bill web service and spock test

---
+++ grails-app/services/skyunattendedtest/BillService.groovy
@@ -0,0 +1,16 @@
+package skyunattendedtest
+
+import groovy.json.JsonSlurper
+
+class BillService {
+
+    def fetchBill( String url ) {
+        new JsonSlurper().parseText( callService( url ) )
+    }
+
+    String callService( String url ) {
+        new URL( url ).text
+    }
+
+
+}
