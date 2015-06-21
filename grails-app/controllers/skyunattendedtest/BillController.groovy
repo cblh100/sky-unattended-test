06a20892d14d9ed1e93f4e7a7dcfc15452a807b9
Chris Leeds-Harrison <chris@megadome.co.uk>
2015-06-21 12:50:40 +0100

    Added a few comments to the source

--- grails-app/controllers/skyunattendedtest/BillController.groovy
+++ grails-app/controllers/skyunattendedtest/BillController.groovy
@@ -4,6 +4,10 @@ class BillController {
 
   BillService billService
 
+  /**
+   * Fetches the customer bill via the bill service and returns it to
+   * the index view to be rendered
+   */
   def index() {
     [bill: billService.fetchBill('http://safe-plains-5453.herokuapp.com/bill.json')]
   }
--- grails-app/taglib/skyunattendedtest/SkyTagLib.groovy
+++ grails-app/taglib/skyunattendedtest/SkyTagLib.groovy
@@ -5,10 +5,17 @@ import java.text.DecimalFormat
 class SkyTagLib {
   static defaultEncodeAs = [taglib: 'html']
   static namespace = 'sky'
+
+  /**
+   * Formats a date string from yyyy-MM-dd to d MMM yyyy
+   */
   def formatDate = { attrs, body ->
     out << Date.parse('yyyy-MM-dd', attrs.date).format('d MMM yyyy')
   }
 
+  /**
+   * Formats a number to a currency representation. i.e '1.2' -> '£1.20'
+   */
   def formatAmount = { attrs, body ->
     out << DecimalFormat.getCurrencyInstance(Locale.UK).format(attrs.amount)
   }
